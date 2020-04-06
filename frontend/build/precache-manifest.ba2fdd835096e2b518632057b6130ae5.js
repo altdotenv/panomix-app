a592b842b6c54e4268285b059b4a86d4b858150b
Jisu Han <jisuhan@Jisuui-MacBook-Pro.local>
2020-04-06 16:27:53 +0900

    add homepage in pakage.json

---
+++ frontend/build/precache-manifest.ba2fdd835096e2b518632057b6130ae5.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8832ff62a99750cae0f77c1ffe2ddbeb",
+    "url": "https://panomixapp.s3.amazonaws.com/index.html"
+  },
+  {
+    "revision": "953ed574c1e538347b2b",
+    "url": "https://panomixapp.s3.amazonaws.com/static/css/2.bf33bae2.chunk.css"
+  },
+  {
+    "revision": "d847f22756a405680e96",
+    "url": "https://panomixapp.s3.amazonaws.com/static/css/main.e13f85d0.chunk.css"
+  },
+  {
+    "revision": "953ed574c1e538347b2b",
+    "url": "https://panomixapp.s3.amazonaws.com/static/js/2.3a76ea33.chunk.js"
+  },
+  {
+    "revision": "6c03bf7d357fb07ee478570c6fa965bf",
+    "url": "https://panomixapp.s3.amazonaws.com/static/js/2.3a76ea33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d847f22756a405680e96",
+    "url": "https://panomixapp.s3.amazonaws.com/static/js/main.c808ba51.chunk.js"
+  },
+  {
+    "revision": "276ede75fc425afa4abe",
+    "url": "https://panomixapp.s3.amazonaws.com/static/js/runtime-main.9c61a787.js"
+  },
+  {
+    "revision": "f8fdd13bdc87aae468aa8a1c1650625c",
+    "url": "https://panomixapp.s3.amazonaws.com/static/media/01.f8fdd13b.png"
+  },
+  {
+    "revision": "5469143a2bbfdb84aeb7d528b8d165c4",
+    "url": "https://panomixapp.s3.amazonaws.com/static/media/02.5469143a.png"
+  },
+  {
+    "revision": "8aa387681f139f8888ea15433323ddbf",
+    "url": "https://panomixapp.s3.amazonaws.com/static/media/03.8aa38768.png"
+  },
+  {
+    "revision": "ce661adcdd0a69df519650a1d2e4a666",
+    "url": "https://panomixapp.s3.amazonaws.com/static/media/09.ce661adc.png"
+  },
+  {
+    "revision": "b531b3e1bb22d330e660b99401ecacee",
+    "url": "https://panomixapp.s3.amazonaws.com/static/media/10.b531b3e1.png"
+  },
+  {
+    "revision": "d09d0cd7c9b29953317248e869590600",
+    "url": "https://panomixapp.s3.amazonaws.com/static/media/11.d09d0cd7.png"
+  },
+  {
+    "revision": "164991cfcba557d27f0dd63bc10f4ef8",
+    "url": "https://panomixapp.s3.amazonaws.com/static/media/image-02.164991cf.png"
+  },
+  {
+    "revision": "3f467f73c34e21fcf34a0f514178754c",
+    "url": "https://panomixapp.s3.amazonaws.com/static/media/image-03.3f467f73.png"
+  },
+  {
+    "revision": "ce64ec9772ed0eb95351bf70d1fc44be",
+    "url": "https://panomixapp.s3.amazonaws.com/static/media/image-04.ce64ec97.png"
+  },
+  {
+    "revision": "716f264aff26255e5133be44a16e1790",
+    "url": "https://panomixapp.s3.amazonaws.com/static/media/image-05.716f264a.png"
+  },
+  {
+    "revision": "e0a0fa0f42dff57f00c079fc661c3562",
+    "url": "https://panomixapp.s3.amazonaws.com/static/media/logo_google ads.e0a0fa0f.png"
+  },
+  {
+    "revision": "fe8f840069e77bc40e2d8bb5eb4877ab",
+    "url": "https://panomixapp.s3.amazonaws.com/static/media/logo_google analytics.fe8f8400.png"
+  },
+  {
+    "revision": "463f0ca531c745d47b06ee46cddf3687",
+    "url": "https://panomixapp.s3.amazonaws.com/static/media/logo_slack.463f0ca5.png"
+  },
+  {
+    "revision": "7454ad30f4d59b60ef1751c4a9d509c1",
+    "url": "https://panomixapp.s3.amazonaws.com/static/media/panomix_logo_mix-06.7454ad30.png"
+  }
+]);
